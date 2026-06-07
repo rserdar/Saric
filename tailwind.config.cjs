cdfd95cf0d0c789f9092154f3275d6f9c025c8e5
Serdar Yavuz <r.serdar@gmail.com>
2026-06-07 23:00:26 +0300

    Add PostCSS and Tailwind config, update Progressbar component and Tailwind_Theme.xml

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./src/**/*.{astro,js,ts,jsx,tsx,css}",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
