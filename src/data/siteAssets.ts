c10e2daf5b528ba492881f1e504b8dd3efaa2841
Serdar Yavuz <r.serdar@gmail.com>
2026-06-09 20:19:54 +0300

    feat: logo dosyalari eklendi, Header ve siteAssets guncellendi

--- src/data/siteAssets.ts
+++ src/data/siteAssets.ts
@@ -1,11 +1,13 @@
 import avatar from '../assets/img/slider/avatar.webp';
-import logoMain from '../assets/img/logo/logo.webp';
+import logoLight from '../assets/img/logo/Logo_light.webp';
+import logoDark from '../assets/img/logo/Logo_dark.webp';
 
 export const siteAssets = {
   hero: {
     avatar,
   },
   logo: {
-    main: logoMain,
+    light: logoLight,
+    dark: logoDark,
   },
 } as const;
